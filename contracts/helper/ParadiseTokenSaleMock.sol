08aebbd361c7e6d712b1e832c683e1874fd9532b
Paradise Token <info@paradisetoken.co.uk>
2018-06-29 18:46:43 +0200

    Update ParadiseTokenSaleMock.sol

--- contracts/helper/ParadiseTokenSaleMock.sol
+++ contracts/helper/ParadiseTokenSaleMock.sol
@@ -1,1 +1,43 @@
+pragma solidity ^0.4.21;
 
+import '../ParadiseTokenSale.sol';
+
+/**
+ * The ParadiseToken token (PDT) has a fixed supply and restricts the ability
+ * to transfer tokens until the owner has called the enableTransfer()
+ * function.
+ *
+ * The owner can associate the token with a token sale contract. In that
+ * case, the token balance is moved to the token sale contract, which
+ * in turn can transfer its tokens to contributors to the sale.
+ */
+ 
+ contract ParadiseTokenSaleMock is ParadiseTokenSale {
+ 
+ uint public _now;
+ 
+ constructor(
+        address ifSuccessfulSendTo,
+        uint fundingGoalInEthers,
+        uint fundingCapInEthers,
+        uint minimumContributionInWei,
+        uint start,
+        uint durationInMinutes,
+        uint ratePDTToEther,
+        address addressOfTokenUsedAsReward
+    ) ParadiseTokenSale(ifSuccessfulSendTo, fundingGoalInEthers, fundingCapInEthers,
+                     minimumContributionInWei, start, durationInMinutes, ratePDTToEther,
+                     addressOfTokenUsedAsReward){ 
+        _now = start + 1;
+    }
+
+    function currentTime() constant public returns (uint) {
+        return _now;
+    }
+
+    event HitLine(uint key, uint val);
+    function changeTime(uint _newTime) onlyOwner external {
+        HitLine(123, _newTime);
+        _now = _newTime;
+    }
+}
