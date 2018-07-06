d9fed12a7161032264895f3e96b81140fb7de712
Paradise Token <info@paradisetoken.co.uk>
2018-07-06 19:49:06 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,14 +1,19 @@
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = "<  >"; // Get yours at https://infura.io/signup. It's free.
+var mnemonic = "< REPLACE THIS WITH YOUR METAMASK SEED PHRASES >"; 
+
 module.exports = {
   networks: {
-    localhost: {
-      host: "localhost", 
-      port: 8545,
-      network_id: "*" 
-    },  
-    ropsten: {
+    development: {
       host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten:  {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
       port: 8546,
-      network_id: "3"
+      network_id: 3,
     }
-  }
+  },
 };
