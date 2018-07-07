be50e75e2ea0c7131f6a32a616952a6b1eb4467c
Paradise Token <info@paradisetoken.co.uk>
2018-07-07 12:40:45 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -6,13 +6,11 @@ var mnemonic = "< REPLACE THIS WITH YOUR METAMASK SEED PHRASES >";
 module.exports = {
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
+      provider: new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/"),
       network_id: "*" // Match any network id
     },
     ropsten:  {
       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
-      port: 8546,
       network_id: 3,
     }
   },
